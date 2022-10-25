e170af26dd3f46ec57c269e8871d3f5fc0c9ba1c
motoco <octavian.motoc@msg.group>
2022-10-25 21:20:44 +0300

    Added tabs & icons for camera, home, and messages sections

--- app/src/main/java/com/example/myapplication/Main/MainActivity.java
+++ app/src/main/java/com/example/myapplication/Main/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication.Main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.view.MenuItem;
 import com.example.myapplication.R;
 import com.example.myapplication.utils.BottomNavigationViewHelper;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,9 +28,33 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: starting.");
 
         setupBottomNavigationView();
+        setupViewPager();
     }
 
-    // BottomNavigationView setup
+    /**
+     * responsible for adding the 3 tabs: Camera, Home, Messages
+     */
+    private void setupViewPager() {
+        SectionsPagerAdapter adapter =
+                new SectionsPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new CameraFragment()); // index 0
+        adapter.addFragment(new MainFragment()); // index 1
+        adapter.addFragment(new MessagesFragment()); // index 2
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_action_name);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_arrow);
+    }
+    /**
+     * BottomNavigationView setup
+     */
     private void setupBottomNavigationView() {
         Log.d(TAG, "settingUpBottomNavigationView");
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavViewBar);
